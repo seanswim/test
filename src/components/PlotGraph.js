b9aeadaf842e92c03b5c0b3d5745bd471b84a603
Sean Kim <sean@fairlabs.io>
2022-11-07 19:55:59 +0900

    Create blog page #F10D-1074

---
+++ src/components/PlotGraph.js
@@ -0,0 +1,19 @@
+import Plot from 'react-plotly.js';
+
+export const PlotGraph = ({data, layout}) => {
+  return (
+    <>
+      ???
+      <Plot
+        data={data}
+        layout={layout}
+        // frames={this.state.frames}
+        // config={this.state.config}
+        // onInitialized={(figure) => this.setState(figure)}
+        // onUpdate={(figure) => this.setState(figure)}
+      />
+    </>
+  )
+};
+
+export default PlotGraph;
