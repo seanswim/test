23e395363e7cd49f744539cca7409373586d4dcb
Sean Kim <sean@fairlabs.io>
2022-11-11 16:50:54 +0900

    fix unoptimize image

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    unoptimized: true,
+  }
 }
 
 module.exports = nextConfig
