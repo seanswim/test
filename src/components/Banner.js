07b148aeee7c5ba628c49d1954784d64fe75b00d
Sean Kim <sean@fairlabs.io>
2022-11-15 12:21:42 +0900

    fix banner texts

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,13 +1,19 @@
-import { Stack, Typography } from '@mui/material';
+import { Stack, Typography, Chip } from '@mui/material';
 
 const Banner = () => {
   return (
     <Stack sx={{textAlign: 'center'}} mb='20px'>
-      <Typography sx={{fontSize: '48px', fontWeight: '700', mt: '40px', mb: '20px'}}>
-        Lorem ipsum dolor sit amet.
+      <div>
+        <Chip label='Beta 1.0' color='primary' sx={{fontSize: '16px', mt: '40px', mb: '10px'}} />
+      </div>
+      <Typography sx={{fontSize: '48px', fontWeight: '700', mt: '10px', mb: '20px'}}>
+        ESG Lens Reports
+      </Typography>
+      <Typography sx={{fontSize: '16px', color: '#6B87A2', mb: '10px'}}>
+        ESG Lens Reports is a new kind of data-driven reporting interface
       </Typography>
       <Typography sx={{fontSize: '16px', color: '#6B87A2', mb: '40px'}}>
-        Id magnam porro aut debitis autem sit dolorem soluta ea dolorum repellat. Sit placeat quia eos modi sint sed ducimus iste ut laboriosam repellendus sit iste laboriosam?
+        that’s built for creating evidence-based, sharable, and reusable reports linked to ESG Lens data and analytics.
       </Typography>
     </Stack>
   )
